5857e14222086e77fc1c4f441ffa2a10fb39a31d
dasdawg428 <66803123+joeydangle568@users.noreply.github.com>
2020-06-16 13:46:24 -0500

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+author 'Dasdawg428'
+description 'Rules command for tethered optic'
+version '1.0.0'
+
+-- What to run
+client_scripts {
+    'client.lua'
+}
