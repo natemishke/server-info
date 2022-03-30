136c210d815172b81d6ed154dfffc62c8a7dcf86
dasdawg428akadoug <66803123+natemishke@users.noreply.github.com>
2022-03-30 00:58:32 -0500

    Updated Fxmanifest
    Updated fxmanifest description

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 games {'gta5'}
 
 author 'Dasdawg428'
-description 'Rules command for tethered optic'
+description 'Rules command for Texas State Roleplay'
 version '1.0.0'
 
 -- What to run
