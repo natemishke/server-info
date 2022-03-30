75a854bf382b1eba4b74dbb1577c6803b31e2b76
dasdawg428akadoug <66803123+natemishke@users.noreply.github.com>
2022-03-30 00:59:05 -0500

    Update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 games {'gta5'}
 
 author 'Dasdawg428'
-description 'Rules command for Texas State Roleplay'
+description 'Discord command for Texas State Roleplay'
 version '1.0.0'
 
 -- What to run
