3452fb109abe07c78390c670378b5998acd7eaab
dasdawg428akadoug <66803123+natemishke@users.noreply.github.com>
2022-07-14 22:22:53 -0500

    Create client-looped.lua

---
+++ client-looped.lua
@@ -0,0 +1,22 @@
+-- Register new command /help
+Citizen.CreateThread(function()
+    while true do
+        msg("Discord: https://discord.gg/qPdaxJ9DhZ")
+        Citizen.Wait(5 * 1000)
+    end
+    -- when /help i used the following msg will be printed to client
+    -- This is just where you would put your discord, website, etc. and/or any message you would like to send
+end, false)
+
+--Function with the new event trigger
+function msg(text)
+    -- Triggering new event chat:addMessage instead of depreceated "chatMessage"
+    TriggerEvent("chat:addMessage", {
+        -- Color of the msg printer
+        color = { 255, 0, 0 }, -- This is RGB. Remember, rgb only allows up to but not over 255,255,255
+        -- Allowed to use more than 1 line? true/false. It's recommended that you copy+paste this and always use multiline true
+        multiline = true,
+        -- arguments = {author, otherargs...}
+        args = { "[Server]", text }
+    })
+end
